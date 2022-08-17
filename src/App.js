f83d615adb8e5a1c90006288f7aa027c18df43e9
Ashwin U Iyer <ashwiniyer1706@gmail.com>
2022-08-18 01:55:23 +0530

    Created calculation for 9+ CGPA

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import InputForm from "./components/InputForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState, useEffect } from "react";
+
+export default function App() {
+  const [data, setData] = useState({
+    CGPA: 0.0,
+    creditsDone: 0,
+    creditsRegistered: 0,
+    changed:false,
+    requiredGPA:0
+  });
+  
+  useEffect(() => {
+    if(data.changed){
+      let newCGPA=9
+      let requiredGPA=0.0
+      requiredGPA=newCGPA*(data.creditsDone+data.creditsRegistered)
+      requiredGPA-=data.creditsDone*data.CGPA
+      requiredGPA/=data.creditsRegistered
+      setData({requiredGPA:requiredGPA})
+    }
+    
+  }, [data]);
+  if (data.requiredGPA==0){
+    return (
+      <div className="layout">
+        <h1>GPA Calculator!</h1>
+        <InputForm setData={setData} />
+      </div>
+    );}
+  else {
+    return (
+      <div className="layout">
+        <h1>GPA Calculator!</h1>
+        <InputForm setData={setData} />
+        <h2> {data.requiredGPA} </h2>
+      </div>
+    );
+    }
+
 }
-
-export default App;
---
+++ src/components/InputForm.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "../App.css";
+
+export default function InputForm(props) {
+  const handleChange = (event) => {
+    var data = {
+      CGPA: parseFloat(event.target.elements.cgpa.value),
+      creditsDone: parseInt(event.target.elements.creditsDone.value),
+      creditsRegistered: parseInt(
+        event.target.elements.creditsRegistered.value
+      ),
+      changed:true
+    };   
+    props.setData(data);
+    event.preventDefault();
+  };
+  return (
+    <div className="layout">
+      <form onSubmit={handleChange}>
+      <div className="form-element">
+        <label htmlFor="cgpa">GPA</label>
+        <input name="cgpa" type="text"></input>
+      </div>
+      <div className="form-element">
+        <label htmlFor="creditsDone">Credits Done</label>
+        <input name="creditsDone" type="text"></input>
+      </div>
+      <div className="form-element">
+        <label htmlFor="creditsRegistered">Credits Registered</label>
+        <input name="creditsRegistered" type="text"></input>
+      </div>
+      <div className="form-element">
+        <button type="submit">Submit</button>
+      </div>
+      </form>
+    </div>
+  );
+}
